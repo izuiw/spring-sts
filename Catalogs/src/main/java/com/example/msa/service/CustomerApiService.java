7d363ddf977f97bf7ed30d5b76e5e7dd307d85a0
Minji <aswll.kr@gmail.com>
2022-03-21 22:04:25 +0900

    220321 -sts 연동

---
+++ Catalogs/src/main/java/com/example/msa/service/CustomerApiService.java
@@ -0,0 +1,6 @@
+package com.example.msa.service;
+
+
+public interface CustomerApiService {
+	String getCustomerDetail(String customerId);
+}
---
+++ Customers/src/main/java/com/example/msa/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.example.msa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+	@GetMapping("/{customerId}")
+	public String getCustomerDetail(@PathVariable String customerId) {
+		
+		System.out.println("reeuqest customerId : " + customerId);
+		return "[Customer Id =" + customerId + " at " + System.currentTimeMillis() + "]";
+	}
+}
